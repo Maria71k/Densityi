72b3bb0cb487375844fcefeea5e0a278800843f0
Maria71k <149970790+Maria71k@users.noreply.github.com>
2024-02-28 16:41:45 +0100

    Add files via upload

---
+++ Densityi.py
@@ -0,0 +1,8 @@
+def calculate_traffic_density(num_cars, road_length):
+    return num_cars / road_length
+
+# Example usage:
+num_cars = 50
+road_length = 500
+traffic_density = calculate_traffic_density(num_cars, road_length)
+print("Traffic density:", traffic_density)
